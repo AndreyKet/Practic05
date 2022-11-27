34573eed1584b1da8b3170076386a649554c9073
Andrey <man.liher66@gmail.com>
2022-11-27 18:19:48 +0300

    ДЗ_Задача№3

---
+++ ДЗ_Задача№3/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 3: Задайте массив из 8 случайных чисел.
+// Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+
+// 4; массив [6, 7, 19, 345, 3] -> нет
+
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+// Вариант со статичным заданым массивом
+
+
+
+// int[] array = {6, 7, 19, 345, 3, 6, 25, 67};
+// int chekNum = 3;
+// int sum = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] == chekNum)
+//     {
+//         sum = sum + 1;
+//     }
+// }
+// if(sum == 0)
+// {
+//     Console.WriteLine("Нет");
+// }
+// else
+// {
+//     Console.WriteLine("Есть");
+// }
+
+
+// Вариант с рандомно генерируемым списком
+
+Console.Write("Ведите число: ");
+string str = Console.ReadLine();
+int chekNum = int.Parse(str);
+int[] array = new int [8];
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    int randomNumber = new Random().Next(0, 100);
+    array[i] = randomNumber;
+    Console.Write(array[i] + ", ");
+
+    if (array[i] == chekNum)
+    {
+        sum = sum + 1;
+    }
+}
+if(sum == 0)
+{
+    Console.WriteLine();
+    Console.WriteLine("Нет");
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Есть");
+}
