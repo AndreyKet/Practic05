f5d9c660c1514d54200f09b11a0f4bbadb6c77b2
Andrey <man.liher66@gmail.com>
2022-11-27 04:45:18 +0300

    ДЗ_Задача№38

---
+++ ДЗ_Задача№38/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между 
+// максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+int[] array = {3, 7, 22, 2, 78};
+int minNum = array[0];
+int maxNum = array[0];
+int diffNum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < minNum)
+    {
+        minNum = array[i];
+    }
+    if (array[i] > maxNum)
+    {
+        maxNum = array[i];
+    }
+    diffNum = maxNum - minNum;
+}
+Console.WriteLine("Разница между Max/Min: "+ diffNum);
